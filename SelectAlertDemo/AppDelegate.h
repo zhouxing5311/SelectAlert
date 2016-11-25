1993321d565267114ed6b4b889f1aa7b0801851d
小九投资 <xiaojiutouzi@xiaojiutouzideiMac.local>
2016-11-25 13:11:24 +0800

    init

---
+++ SelectAlertDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectAlertDemo
+//
+//  Created by apple on 2016/11/25.
+//  Copyright © 2016年 周兴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
